5b3a6e44067562ff8e4d21e6117f8ee52d2b24df
Lorenzo Greco <lorenzo.rev@gmail.com>
2014-02-25 20:20:01 +0100

    refactoring, add comments, add options

--- leaflet-draggable-polyline.js
+++ leaflet-draggable-polyline.js
@@ -3,17 +3,24 @@ L.EditDrag = L.EditDrag || {};
 L.EditDrag.Polyline = L.Handler.extend({
 
   options: {
-    distance: 30,
+    distance: 30,   //distance from pointer to the polyline
+    tollerance: 5,  //tollerance for snap effect to vertex
+    draggables: {
+      first: true,      //first vertex is draggable
+      middle: true,     //middle vertices are draggables
+      last: true,       //last vertex draggable
+      add_vertex: true  //define if the number of polyline's vertices can change
+    },
     icon: new L.DivIcon({
       iconSize: new L.Point(8, 8),
       className: 'leaflet-div-icon leaflet-editing-icon'
     })
   },
 
-  initialize: function(poly, options) {
+  initialize: function(poly) {
     this._poly = poly;
     this._marker = null;
-    L.setOptions(this, options);
+    L.Util.setOptions(this, poly.options);
   },
 
   addHooks: function() {
@@ -27,58 +34,86 @@ L.EditDrag.Polyline = L.Handler.extend({
     this._map.off('mousemove');
   },
 
+  /**
+  * return the closest point on the closest segment
+  */
+  _getClosestPointAndSegment: function(latlng) {
+    var distanceMin = Infinity;
+    var segmentMin = null;
+
+    for (var i = 0, len = (this._poly._latlngs.length -1); i < len; i++) {
+      var segment = [ this._poly._latlngs[i], this._poly._latlngs[i + 1] ];
+      var distance = L.GeometryUtil.distanceSegment(this._map, latlng, segment[0], segment[1]);
+      if (distance < distanceMin) {
+        distanceMin = distance;
+        segmentMin = segment;
+      }
+    }
+
+    return { point: L.GeometryUtil.closestOnSegment(this._map, latlng, segmentMin[0], segmentMin[1]) , segment: segmentMin };
+  },
+
   _mouseContextClick: function(e) {
-    var closestPoint = L.GeometryUtil.closest(this._map, this._poly, e.target.getLatLng(), true);
-    this._poly._latlngs.splice(this._poly._latlngs.indexOf(closestPoint), 1);
-    this._poly.redraw();
+    var closest = L.GeometryUtil.closest(this._map, this._poly, e.latlng, true);
+
+    if (closest.distance < this.options.tollerance) {
+      this._poly.spliceLatLngs(this._poly._latlngs.indexOf(closest), 1);
+    }
   },
 
   _mouseMove: function(e) {
     var closest = L.GeometryUtil.closestLayerSnap(this._map, [this._poly], e.latlng, this.options.distance, false);
 
-    if (this._marker) {
-      if (closest) {
-        this._marker.addTo(this._map);
-        L.extend(this._marker._latlng, closest.latlng);
-        this._marker.update();
-      } else {
-        this._map.removeLayer(this._marker)
-      }
-    } else {
-      if (!closest) return;
+    if (this._marker && closest) {
+      this._marker.addTo(this._map);
+      L.extend(this._marker._latlng, closest.latlng);
+      this._marker.options.draggable = true;
+      this._marker.update();
+
+    } else if (!this._marker && closest) {
       this._marker = L.marker(closest.latlng, { draggable: true, icon: this.options.icon }).addTo(this._map);
       this._marker.on('dragstart', this._markerDragStart, this);
       this._marker.on('drag', this._markerDrag, this);
-      this._marker.on('contextmenu', this._mouseContextClick, this);
       this._marker.on('dragend', this._markerDragEnd, this);
+      this._marker.on('contextmenu', this._mouseContextClick, this);
+
+    } else if (this._marker) {
+      this._map.removeLayer(this._marker);
+      this._marker = null;
     }
   },
 
-  _markerDragStart: function(e) {
-    var marker = e.target;
+  _disableDrag: function() {
+    this.closest = null;
+    this._marker.options.draggable = false;
+  },
 
-    this.closest = L.GeometryUtil.closest(this._map, this._poly, marker.getLatLng(), true);
+  _markerDragStart: function(e) {
+    var latlng = e.target.getLatLng();
+
+    this.closest = L.GeometryUtil.closest(this._map, this._poly, latlng, true);
+    var index = this._poly._latlngs.indexOf(this.closest);
+    var polylength = this._poly._latlngs.length;
+
+    if ((!this.options.draggables.first && index == 0) ||
+        (!this.options.draggables.last && index == (polylength -1)) ||
+        (!this.options.draggables.middle && (index > 0 && index < (polylength - 1)))) {
+        this._disableDrag();
+        return;
+    }
 
     //set a tollerance of 5
-    if (this.closest.distance > 5) {
+    if (this.closest.distance > this.options.tollerance) {
 
-      //locate the closest point on the closest segment
-      var distanceMin = Infinity;
-      var segmentMin = null;
-
-      for (var i = 0, len = (this._poly._latlngs.length -1); i < len; i++) {
-
-        var segment = [this._poly._latlngs[i], this._poly._latlngs[i+1]];
-        var distance = L.GeometryUtil.distanceSegment(this._map, marker.getLatLng(), segment[0], segment[1]);
-        if (distance < distanceMin) {
-          distanceMin = distance;
-          segmentMin = segment;
-        }
+      if (!this.options.draggables.add_vertex) {
+        this._disableDrag();
+        return;
       }
 
-      this.closest = L.GeometryUtil.closestOnSegment(this._map, marker.getLatLng(), segmentMin[0], segmentMin[1]);
+      this.closest = this._getClosestPointAndSegment(latlng);
+
       //add a new vertex
-      var insertAt = this._poly._latlngs.indexOf(segmentMin[1]);
+      var insertAt = this._poly._latlngs.indexOf(this.closest.segment[1]);
       this._poly._latlngs.splice(insertAt, 0, this.closest);
     }
 
@@ -86,9 +121,11 @@ L.EditDrag.Polyline = L.Handler.extend({
   },
 
   _markerDrag: function(e) {
-    this.closest.lat = e.target.getLatLng().lat;
-    this.closest.lng = e.target.getLatLng().lng;
-    this._poly.redraw();
+    if (this.closest) {
+      this.closest.lat = e.target.getLatLng().lat;
+      this.closest.lng = e.target.getLatLng().lng;
+      this._poly.redraw();
+    }
   },
 
   _markerDragEnd: function(e) {
@@ -98,14 +135,14 @@ L.EditDrag.Polyline = L.Handler.extend({
 
 L.Polyline.addInitHook(function() {
 
-  if (this.editDrag) {
+  if (this.edit_with_drag) {
     return;
   }
 
   if (L.EditDrag.Polyline) {
     this.editingDrag = new L.EditDrag.Polyline(this);
 
-    if (this.options.editDrag) {
+    if (this.options.edit_with_drag) {
       this.editingDrag.enable();
     }
   }
